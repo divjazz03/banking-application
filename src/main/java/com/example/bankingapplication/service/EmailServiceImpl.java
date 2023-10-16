d9030ea88b0711a0361ef6ff86368f0e0a936fc7
divjazz03 <divjazz03@gmail.com>
2023-10-16 12:57:20 +0100

    ###Added more Email Functionality###

--- src/main/java/com/example/bankingapplication/service/EmailServiceImpl.java
+++ src/main/java/com/example/bankingapplication/service/EmailServiceImpl.java
@@ -1,17 +1,17 @@
 package com.example.bankingapplication.service;
 
 import com.example.bankingapplication.dto.EmailDetails;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+
 @Service
 public class EmailServiceImpl implements EmailService{
-
+    @Autowired
     private JavaMailSender javaMailSender;
 
     @Value("${spring.mail.username}")
